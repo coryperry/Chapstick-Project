013220bd2b51d142276ad60bb8fc030a744a5fa7
Cory Perry <cperry25@gmail.com>
2011-06-21 21:12:02 -0400

    Initial commit of base files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chapstick::Application.config.secret_token = '1f24425a4401126b639103a0086e5519e9ab99374547774875329b5970cc80f2a8d2d9ca726a3f2717575ab437640b8a342f8de19c42c1bfe61a99a40c34aeba'
